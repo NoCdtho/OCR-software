322d8a3f345b69a0b977153d42e073ca99bad288
Rohit Gupta <shecded@gmail.com>
2026-09-19 04:40:36 +0530

    Implement construct table method

--- main.py
+++ main.py
@@ -12,6 +12,38 @@ import time
 import json
 import jiwer
 
+def reconstruct_table(cells, texts):
+
+    # Filter out spanning cells (which have None for row/col) to find grid dimensions
+    grid_cells = [c for c in cells if c.get('row') is not None and c.get('col') is not None]
+
+    if grid_cells:
+        # Find max rows and columns to create our empty 2D array
+        max_row = max(c['row'] for c in grid_cells)
+        max_col = max(c['col'] for c in grid_cells)
+    else:
+        max_row, max_col = 0, 0
+
+    table_data = [['' for _ in range(max_col + 1)] for _ in range(max_row + 1)]
+
+    # Populate the 2D array with our extracted text
+    spanning_texts = []
+    for cell, text in zip(cells, texts): #type: ignore
+        r, c = cell.get('row'), cell.get('col')
+        # If it's a standard grid cell
+        if r is not None and c is not None:
+            table_data[r][c] = text.strip()
+        # If it's a spanning cell
+        else:
+            spanning_texts.append(text.strip())
+
+    # Convert to Pandas DataFrame and Save
+    df = pd.DataFrame(table_data)
+
+    print("\n" + "="*50)
+    print(df.to_string()) # Print the dataframe to terminal
+    print("="*50)
+
 def pipeline(image_path, yolo_weights, crnn_weights, output_csv, ocr_type, use_bart=False):
     print("1. Loading Models...")
 
@@ -28,7 +60,7 @@ def pipeline(image_path, yolo_weights, crnn_weights, output_csv, ocr_type, use_b
 
     # 1.5 Load, Deskew, and Save temporary image
     print("Fixing the image structure...")
-    raw_img = cv2.imread(image_path) # This method load image in numpy array representing its vla
+    raw_img = cv2.imread(image_path) # This method load image in numpy array in pixels 
     if raw_img is None:
         raise FileNotFoundError(f"Cannot load image at {image_path}")
         
@@ -85,40 +117,7 @@ def pipeline(image_path, yolo_weights, crnn_weights, output_csv, ocr_type, use_b
             texts.append(predicted_text)
 
 # Below code is used to reconstruct the words in the terminal and in the CSV files
-    print("4. Reconstructing Table and saving to CSV...")
-    
-    # Filter out spanning cells (which have None for row/col) to find grid dimensions
-    grid_cells = [c for c in cells if c.get('row') is not None and c.get('col') is not None]
-
-    if grid_cells:
-        # Find max rows and columns to create our empty 2D array
-        max_row = max(c['row'] for c in grid_cells)
-        max_col = max(c['col'] for c in grid_cells)
-    else:
-        max_row, max_col = 0, 0
-
-    table_data = [['' for _ in range(max_col + 1)] for _ in range(max_row + 1)]
-
-    # Populate the 2D array with our extracted text
-    spanning_texts = []
-    for cell, text in zip(cells, texts): #type: ignore
-        r, c = cell.get('row'), cell.get('col')
-        # If it's a standard grid cell
-        if r is not None and c is not None:
-            table_data[r][c] = text.strip()
-        # If it's a spanning cell
-        else:
-            spanning_texts.append(text.strip())
-
-    # Convert to Pandas DataFrame and Save
-    df = pd.DataFrame(table_data)
-
-    print("\n" + "="*50)
-    print(df.to_string()) # Print the dataframe to terminal
-    print("="*50)
-
-    df.to_csv(output_csv, index=False, header=False) # No headers, just raw data
-    print(f"\n SUCCESS: Table saved to {output_csv}")
+    reconstruct_table(cells, texts)
     return texts
 
 def evaluate_prediction(ground_truth_list, predicted_list):
@@ -145,10 +144,8 @@ def average(processing_times, image_files, total_cer_list, total_wer_list):
     if total_cer_list and total_wer_list:
         avg_cer = sum(total_cer_list) / len(total_cer_list)
         avg_wer = sum(total_wer_list) / len(total_wer_list)
-        print("-" * 50)
-        print(f"Average Pipeline CER: {avg_cer*100:.2f}%")
-        print(f"Average Pipeline WER: {avg_wer*100:.2f}%")
-
+        print()
+        print(f"Average Pipeline CER and WER for all images are {avg_cer*100:.2f}% and {avg_wer*100:.2f}% respectively")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="End-to-End Table OCR Pipeline")
@@ -211,7 +208,7 @@ if __name__ == "__main__":
 
         print()
 
-        print("Now evaluating the result......")
+        print("Now calculating the error rate the result......")
         # Calculate CER and WER if ground truth exists for this image
         cer, wer = evaluate_prediction(ground_truths[img_name], predicted_words_list)
         total_cer_list.append(cer)
